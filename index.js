1ce007ce99e99028ac8a1fc7135274740cc12e1c
ScProject-Lab <spdev.3141@gmail.com>
2026-02-21 21:48:42 +0900

    サイドパネルの背景色を追加し、最大震度の更新機能を強化

--- index.js
+++ index.js
@@ -34,3 +34,49 @@ $.getJSON(" https://api.p2pquake.net/v2/history?codes=551", function (data) {
     })
     var hypoIcon = L.marker(hypoLatLng, {icon: hypoIconimage }).addTo(map);
 });
+
+
+const latest_maxscale = document.querySelector(".latest-card_maxscale");
+
+function updateMaxScale(scale) {
+    
+    const scaleClassMap = {
+        "7": "seven-bg",
+        "6強": "six-plus-bg",
+        "6弱": "six-minus-bg",
+        "5強": "five-plus-bg",
+        "5弱": "five-minus-bg",
+        "4": "four-bg",
+        "3": "three-bg",
+        "2": "two-bg",
+        "1": "one-bg"
+    };
+
+    // 既存の震度色全消し
+    Object.values(scaleClassMap).forEach(cls => {
+        latest_maxscale.classList.remove(cls);
+    });
+
+    // scaleに対応したクラスを追加
+    const bgClass = scaleClassMap[scale];
+    if (bgClass) {
+        latest_maxscale.classList.add(bgClass);
+    }
+
+    // 数字と修飾に分割
+    const match = scale.match(/^(\d)([^\d]*)$/);
+    const number = match ? match[1] : scale;
+    const modifier = match ? match[2] : "";
+
+    // 更新
+    const txt = latest_maxscale.querySelector(".latest-card_maxscale-txt");
+    const label = latest_maxscale.querySelector(".latest-card_maxscale-label")
+    txt.innerHTML = `${number}<span class="scale_modifier">${modifier}</span>`;
+
+    if (number === "3" || number === "4") {
+        txt.style.color = "#000";
+        label.style.color = "#000";
+    }
+}
+
+updateMaxScale("7");
