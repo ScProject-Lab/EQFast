ab058666b72adc10d80c028025f7173a0ae1bd54
ScProject-Lab <spdev.3141@gmail.com>
2026-02-21 13:37:28 +0900

    スタイルの改善とサイドパネルの追加

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ L.control.scale({ maxWidth: 150, position: 'bottomright', imperial: false }).add
 map.zoomControl.setPosition('topright');
 
 var PolygonLater_Style = {
-    "color": "#ffffff",
+    "color": "#dde0e5",
     "weight": 1.5,
     "opacity": 0.25,
-    "fillColor": "#201d1a",
+    "fillColor": "#32353a",
     "fillOpacity": 1
 }
 
@@ -15,3 +15,22 @@ $.getJSON("prefectures.geojson", function (data) {
         style: PolygonLater_Style
     }).addTo(map);
 }); 
+
+$.getJSON(" https://api.p2pquake.net/v2/history?codes=551", function (data) {
+    var [time, name, shindo, magnitude, depth] = [
+        data["0"]["earthquake"]["time"],
+        data["0"]["earthquake"]["hypocenter"]["name"],
+        data["0"]["earthquake"]["maxScale"],
+        data["0"]["earthquake"]["hypocenter"]["magnitude"],
+        data["0"]["earthquake"]["hypocenter"]["depth"]
+    ]
+
+    var hypoLatLng = new L.LatLng(data[0]["earthquake"]["hypocenter"]["latitude"], data[0]["earthquake"]["hypocenter"]["longitude"]);
+    var hypoIconimage = L.icon({
+        iconUrl: 'source/shingen.png',
+        iconSize: [40,40],
+        iconAnchor: [20, 20],
+        popupAnchor: [0, -40]
+    })
+    var hypoIcon = L.marker(hypoLatLng, {icon: hypoIconimage }).addTo(map);
+});
